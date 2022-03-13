8d443cfcd3f31260ce93796bc67bda52bcf037a8
Padungkiat <aomsinkaky14@gmail.com>
2022-03-13 14:36:40 +0700

    create web-application

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+var posts = [];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/',function(req,res){
+  res.render('home',{
+  });
+});
+app.get('/booking',function(req,res){
+  res.render('booking',{
+  });
+});
+app.get('/category',function(req,res){
+  res.render('category',{
+  });
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+
